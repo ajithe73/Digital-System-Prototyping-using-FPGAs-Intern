14f43757ec835aa3bd2760bb5012bddb0c77161a
AthiRamRS <143102635+ARX-0@users.noreply.github.com>
2024-07-08 07:39:11 +0530

    Update custom_module.v

--- verilog miniproject/custom_module.v
+++ verilog miniproject/custom_module.v
@@ -1,8 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07.07.2024 15:51:33
+// Design Name: 
+// Module Name: custom_module
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module custom_module (
     input clk,
     input reset,
     input [1:0] select,
-    input serial_in,
+    input [2:0] serial_in,
     input [7:0] parallel_in,
     output reg [7:0] parallel_output 
 );
@@ -16,14 +38,14 @@ module custom_module (
         end else begin
             case (select)
                 2'b00: begin
-                    parallel_output <= {serial_in, parallel_in[7:1]}; // shift right
+                    parallel_output <= {serial_in, parallel_in[7:3]}; // shift right LSB is discarded 
                 end
                 2'b01: begin
-                    parallel_output <= {parallel_in[6:0], serial_in}; // shift left
+                    parallel_output <= {parallel_in[4:0], serial_in}; // shift left MSB is discarded
                 end
                 2'b10: begin
                     temp <= temp << 1;
-                    temp[0] <= serial_in;
+                    temp[0] <= serial_in[2]; // only feed the MSB of the serial_in to show how one bit is fed 
                     parallel_output <= temp; // serial in parallel out  
                 end
                 2'b11: begin
@@ -37,4 +59,3 @@ module custom_module (
     end
 
 endmodule
-
